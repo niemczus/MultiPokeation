185e2658a74d58383d03f7dec256563cbe0b869d
niemczus <niemczus@gmail.com>
2022-04-12 17:40:34 +0200

    Clean Up

--- MultiPokeation/Models/Statics.swift
+++ MultiPokeation/Models/Statics.swift
@@ -8,18 +8,6 @@
 import Foundation
 import PokemonAPI
 
-//struct S {
-//    static let username = "username"
-//    static let pokemonName = "pokemonName"
-//    static let pokemonNumber = "pokemonNumber"
-//    static let firstChoose = "firstChoose"
-//    static let evolutionNumber = "evolutionNumber"
-//    static let pokemon = "pokemon"
-//    static let collectionImages = "collectionImages"
-//    static let collectionNames = "collectionNames"
-//    static let score = "score"
-//}
-
 enum Static: String {
     case username
     case pokemonName
